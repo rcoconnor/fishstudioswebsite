3344eb1892a8c3ab80a1e14343c5af0e5ae06312
Rory O'Connor <rcoconnor@crimson.ua.edu>
2021-05-25 20:17:18 -0500

    first commit

---
+++ src/pages/index.js
@@ -0,0 +1,13 @@
+import * as React from "react"
+import Layout from '../components/layout'
+
+const IndexPage = () => {
+    return (
+        <Layout pageTitle="Home Page"> 
+            <p>Im making by following a tutorial</p>
+        </Layout>
+    )
+}
+
+
+export default IndexPage
